0abfcf9320f629a2e46fc1e0017e5d11c0b127b2
prabwastaken <54489997+NotPrab@users.noreply.github.com>
2020-02-27 21:26:24 +0700

    Added

---
+++ Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace FakeObfuscationAttributes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.CfexPro = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.xenocodepro = new System.Windows.Forms.CheckBox();
+            this.smartassemblypro = new System.Windows.Forms.CheckBox();
+            this.agilepro = new System.Windows.Forms.CheckBox();
+            this.dnguardpro = new System.Windows.Forms.CheckBox();
+            this.goliathpro = new System.Windows.Forms.CheckBox();
+            this.yanopro = new System.Windows.Forms.CheckBox();
+            this.cryptopro = new System.Windows.Forms.CheckBox();
+            this.bithelmetpro = new System.Windows.Forms.CheckBox();
+            this.ninerayspro = new System.Windows.Forms.CheckBox();
+            this.mangopro = new System.Windows.Forms.CheckBox();
+            this.dotfuscatorpro = new System.Windows.Forms.CheckBox();
+            this.babelpro = new System.Windows.Forms.CheckBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Location = new System.Drawing.Point(23, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(476, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBox1_DragDrop);
+            this.textBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBox1_DragEnter);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 280);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(476, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CfexPro
+            // 
+            this.CfexPro.AutoSize = true;
+            this.CfexPro.Location = new System.Drawing.Point(9, 9);
+            this.CfexPro.Name = "CfexPro";
+            this.CfexPro.Size = new System.Drawing.Size(80, 17);
+            this.CfexPro.TabIndex = 2;
+            this.CfexPro.Text = "ConfuserEx";
+            this.CfexPro.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(23, 50);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(71, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Check All";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(23, 73);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(465, 201);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.Transparent;
+            this.tabPage1.Controls.Add(this.xenocodepro);
+            this.tabPage1.Controls.Add(this.smartassemblypro);
+            this.tabPage1.Controls.Add(this.agilepro);
+            this.tabPage1.Controls.Add(this.dnguardpro);
+            this.tabPage1.Controls.Add(this.goliathpro);
+            this.tabPage1.Controls.Add(this.yanopro);
+            this.tabPage1.Controls.Add(this.cryptopro);
+            this.tabPage1.Controls.Add(this.bithelmetpro);
+            this.tabPage1.Controls.Add(this.ninerayspro);
+            this.tabPage1.Controls.Add(this.mangopro);
+            this.tabPage1.Controls.Add(this.dotfuscatorpro);
+            this.tabPage1.Controls.Add(this.babelpro);
+            this.tabPage1.Controls.Add(this.CfexPro);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(457, 175);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "#1 ";
+            // 
+            // xenocodepro
+            // 
+            this.xenocodepro.AutoSize = true;
+            this.xenocodepro.Location = new System.Drawing.Point(232, 72);
+            this.xenocodepro.Name = "xenocodepro";
+            this.xenocodepro.Size = new System.Drawing.Size(73, 17);
+            this.xenocodepro.TabIndex = 14;
+            this.xenocodepro.Text = "xenocode";
+            this.xenocodepro.UseVisualStyleBackColor = true;
+            // 
+            // smartassemblypro
+            // 
+            this.smartassemblypro.AutoSize = true;
+            this.smartassemblypro.Location = new System.Drawing.Point(232, 39);
+            this.smartassemblypro.Name = "smartassemblypro";
+            this.smartassemblypro.Size = new System.Drawing.Size(94, 17);
+            this.smartassemblypro.TabIndex = 13;
+            this.smartassemblypro.Text = "smartassembly";
+            this.smartassemblypro.UseVisualStyleBackColor = true;
+            // 
+            // agilepro
+            // 
+            this.agilepro.AutoSize = true;
+            this.agilepro.Location = new System.Drawing.Point(232, 9);
+            this.agilepro.Name = "agilepro";
+            this.agilepro.Size = new System.Drawing.Size(48, 17);
+            this.agilepro.TabIndex = 12;
+            this.agilepro.Text = "agile";
+            this.agilepro.UseVisualStyleBackColor = true;
+            // 
+            // dnguardpro
+            // 
+            this.dnguardpro.AutoSize = true;
+            this.dnguardpro.Location = new System.Drawing.Point(124, 106);
+            this.dnguardpro.Name = "dnguardpro";
+            this.dnguardpro.Size = new System.Drawing.Size(65, 17);
+            this.dnguardpro.TabIndex = 11;
+            this.dnguardpro.Text = "dnguard";
+            this.dnguardpro.UseVisualStyleBackColor = true;
+            // 
+            // goliathpro
+            // 
+            this.goliathpro.AutoSize = true;
+            this.goliathpro.Location = new System.Drawing.Point(124, 140);
+            this.goliathpro.Name = "goliathpro";
+            this.goliathpro.Size = new System.Drawing.Size(57, 17);
+            this.goliathpro.TabIndex = 10;
+            this.goliathpro.Text = "goliath";
+            this.goliathpro.UseVisualStyleBackColor = true;
+            // 
+            // yanopro
+            // 
+            this.yanopro.AutoSize = true;
+            this.yanopro.Location = new System.Drawing.Point(124, 72);
+            this.yanopro.Name = "yanopro";
+            this.yanopro.Size = new System.Drawing.Size(49, 17);
+            this.yanopro.TabIndex = 9;
+            this.yanopro.Text = "yano";
+            this.yanopro.UseVisualStyleBackColor = true;
+            // 
+            // cryptopro
+            // 
+            this.cryptopro.AutoSize = true;
+            this.cryptopro.Location = new System.Drawing.Point(124, 39);
+            this.cryptopro.Name = "cryptopro";
+            this.cryptopro.Size = new System.Drawing.Size(55, 17);
+            this.cryptopro.TabIndex = 8;
+            this.cryptopro.Text = "crypto";
+            this.cryptopro.UseVisualStyleBackColor = true;
+            // 
+            // bithelmetpro
+            // 
+            this.bithelmetpro.AutoSize = true;
+            this.bithelmetpro.Location = new System.Drawing.Point(124, 9);
+            this.bithelmetpro.Name = "bithelmetpro";
+            this.bithelmetpro.Size = new System.Drawing.Size(68, 17);
+            this.bithelmetpro.TabIndex = 7;
+            this.bithelmetpro.Text = "bithelmet";
+            this.bithelmetpro.UseVisualStyleBackColor = true;
+            // 
+            // ninerayspro
+            // 
+            this.ninerayspro.AutoSize = true;
+            this.ninerayspro.Location = new System.Drawing.Point(9, 106);
+            this.ninerayspro.Name = "ninerayspro";
+            this.ninerayspro.Size = new System.Drawing.Size(65, 17);
+            this.ninerayspro.TabIndex = 6;
+            this.ninerayspro.Text = "ninerays";
+            this.ninerayspro.UseVisualStyleBackColor = true;
+            // 
+            // mangopro
+            // 
+            this.mangopro.AutoSize = true;
+            this.mangopro.Location = new System.Drawing.Point(9, 140);
+            this.mangopro.Name = "mangopro";
+            this.mangopro.Size = new System.Drawing.Size(58, 17);
+            this.mangopro.TabIndex = 5;
+            this.mangopro.Text = "mango";
+            this.mangopro.UseVisualStyleBackColor = true;
+            // 
+            // dotfuscatorpro
+            // 
+            this.dotfuscatorpro.AutoSize = true;
+            this.dotfuscatorpro.Location = new System.Drawing.Point(9, 72);
+            this.dotfuscatorpro.Name = "dotfuscatorpro";
+            this.dotfuscatorpro.Size = new System.Drawing.Size(79, 17);
+            this.dotfuscatorpro.TabIndex = 4;
+            this.dotfuscatorpro.Text = "dotfuscator";
+            this.dotfuscatorpro.UseVisualStyleBackColor = true;
+            // 
+            // babelpro
+            // 
+            this.babelpro.AutoSize = true;
+            this.babelpro.Location = new System.Drawing.Point(9, 39);
+            this.babelpro.Name = "babelpro";
+            this.babelpro.Size = new System.Drawing.Size(53, 17);
+            this.babelpro.TabIndex = 3;
+            this.babelpro.Text = "Babel";
+            this.babelpro.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(457, 175);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "#2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(100, 50);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(84, 17);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "Uncheck All";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 321);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FakeObfuscationAttributes";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox CfexPro;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox babelpro;
+        private System.Windows.Forms.CheckBox dotfuscatorpro;
+        private System.Windows.Forms.CheckBox ninerayspro;
+        private System.Windows.Forms.CheckBox mangopro;
+        private System.Windows.Forms.CheckBox dnguardpro;
+        private System.Windows.Forms.CheckBox goliathpro;
+        private System.Windows.Forms.CheckBox yanopro;
+        private System.Windows.Forms.CheckBox cryptopro;
+        private System.Windows.Forms.CheckBox bithelmetpro;
+        private System.Windows.Forms.CheckBox xenocodepro;
+        private System.Windows.Forms.CheckBox smartassemblypro;
+        private System.Windows.Forms.CheckBox agilepro;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using dnlib.DotNet;
+using dnlib.DotNet.Writer;
+
+namespace FakeObfuscationAttributes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region DragDrop
+        public string DirectoryName = "";
+        private void textBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                Array array = (Array)e.Data.GetData(DataFormats.FileDrop);
+                if (array != null)
+                {
+                    string text = array.GetValue(0).ToString();
+                    int num = text.LastIndexOf(".", StringComparison.Ordinal);
+                    if (num != -1)
+                    {
+                        string text2 = text.Substring(num);
+                        text2 = text2.ToLower();
+                        if (text2 == ".exe" || text2 == ".dll")
+                        {
+                            Activate();
+                            textBox1.Text = text;
+                            int num2 = text.LastIndexOf("\\", StringComparison.Ordinal);
+                            if (num2 != -1)
+                            { DirectoryName = text.Remove(num2, text.Length - num2); }
+                            if (DirectoryName.Length == 2)
+                            { DirectoryName += "\\"; }
+                        }
+                    }
+                }
+            }
+            catch { }
+        }
+
+        private void textBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            { e.Effect = DragDropEffects.Copy; }
+            else
+            { e.Effect = DragDropEffects.None; }
+        }
+        #endregion
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Check if the file is empty
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Drag / Drop the file.", "Prab#3268");
+            }
+
+            ModuleDefMD module = ModuleDefMD.Load(textBox1.Text);
+
+            // ConfuserEx
+            if (CfexPro.Checked)
+            {
+                Fake.confuserex(module.Assembly);
+            }
+            // babel
+            if (babelpro.Checked)
+            {
+                Fake.babel(module.Assembly);
+            }
+            // dotfuscator
+            if (dotfuscatorpro.Checked)
+            {
+                Fake.dotfuscator(module.Assembly);
+            }
+            // ninerays
+            if (ninerayspro.Checked)
+            {
+                Fake.ninerays(module.Assembly);
+            }
+            // mango
+            if (mangopro.Checked)
+            {
+                Fake.mango(module.Assembly);
+            }
+            // bithelmet
+            if (bithelmetpro.Checked)
+            {
+                Fake.bithelmet(module.Assembly);
+            }
+            // crypto
+            if (cryptopro.Checked)
+            {
+                Fake.crypto(module.Assembly);
+            }
+            // yano
+            if (yanopro.Checked)
+            {
+                Fake.yano(module.Assembly);
+            }
+            // dnguard
+            if (dnguardpro.Checked)
+            {
+                Fake.dnguard(module.Assembly);
+            }
+            // goliath
+            if (goliathpro.Checked)
+            {
+                Fake.goliath(module.Assembly);
+            }
+            // agile
+            if (agilepro.Checked)
+            {
+                Fake.agile(module.Assembly);
+            }
+            // smartassembly
+            if (smartassemblypro.Checked)
+            {
+                Fake.smartassembly(module.Assembly);
+            }
+            // xenocode
+            if (xenocodepro.Checked)
+            {
+                Fake.xenocode(module.Assembly);
+            }
+
+            #region Path
+            string text2 = Path.GetDirectoryName(textBox1.Text);
+
+            if (!text2.EndsWith("\\"))
+            {
+                text2 += "\\";
+            }
+            string path = text2 + Path.GetFileNameWithoutExtension(textBox1.Text) + "_WithFakeObfuscationAttributes" + Path.GetExtension(textBox1.Text);
+
+            var opts = new ModuleWriterOptions(module);
+
+            opts.PEHeadersOptions.NumberOfRvaAndSizes = 13;
+
+            opts.MetadataOptions.TablesHeapOptions.ExtraData = 0x1337;
+
+            opts.Logger = DummyLogger.NoThrowInstance;
+            module.Write(path, opts);
+            #endregion
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                CfexPro.Checked = true;
+                babelpro.Checked = true;
+                dotfuscatorpro.Checked = true;
+                ninerayspro.Checked = true;
+                mangopro.Checked = true;
+                bithelmetpro.Checked = true;
+                cryptopro.Checked = true;
+                yanopro.Checked = true;
+                dnguardpro.Checked = true;
+                goliathpro.Checked = true;
+                agilepro.Checked = true;
+                smartassemblypro.Checked = true;
+                xenocodepro.Checked = true;
+            }
+
+            if (checkBox1.Checked == false)
+            {
+                clearProtections();
+            }
+        }
+        public void clearProtections()
+        {
+            CfexPro.Checked = false;
+            babelpro.Checked = false;
+            dotfuscatorpro.Checked = false;
+            ninerayspro.Checked = false;
+            mangopro.Checked = false;
+            bithelmetpro.Checked = false;
+            cryptopro.Checked = false;
+            yanopro.Checked = false;
+            dnguardpro.Checked = false;
+            goliathpro.Checked = false;
+            agilepro.Checked = false;
+            smartassemblypro.Checked = false;
+            xenocodepro.Checked = false;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked == true)
+            {
+                clearProtections();
+            }
+        }
+    }
+}
